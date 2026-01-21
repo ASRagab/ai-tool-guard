30cd383c861016677cc8b03a3d509d3e59076fe2
Ahmad Ragab <ahmad.ragab@joinhandshake.com>
2026-01-21 01:12:12 -0500

    Initial commit

--- dist/src/detectors/base-detector.js
+++
@@ -1,6 +0,0 @@
-/**
- * Base detector interfaces for AI tool detection system.
- * Provides consistent contracts for all detector implementations.
- * @module detectors/base-detector
- */
-export {};
